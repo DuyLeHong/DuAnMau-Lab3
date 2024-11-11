107fc424e7f695bdf099e90271057920374d2663
Duy Le <lehongduy.bk@gmail.com>
2024-11-11 10:43:46 +0700

    Initial commit

---
+++ app/src/main/java/datptph27465/fpt/edu/duanmau/Fragment/SachFragment.java
@@ -0,0 +1,21 @@
+package datptph27465.fpt.edu.duanmau.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import datptph27465.fpt.edu.duanmau.R;
+
+public class SachFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sach, container, false);
+    }
+}
